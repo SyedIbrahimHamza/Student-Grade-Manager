5d616441e00666286c1df829e4194c7d88f9daf1
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-08-17 01:07:45 -0700

    Add a basic menu

---
+++ student_grade_manager.py
@@ -0,0 +1,10 @@
+student = []
+while True:
+    print("\n=== STUDENT GRADE MANAGER ===")
+    print("1. Add Student")
+    print("2. View All Students")
+    print("3. Search by Roll Number")
+    print("4. Search by Name")
+    print("5. Class Report (Topper, Average, Fail List)")
+    print("6. Delete Student")
+    print("7. Exit")
